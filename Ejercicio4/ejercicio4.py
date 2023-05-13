6dc5dc9e3c7f1f9e0793f473070b9f2947f1ae9b
Katu97 <skatuchin.97.sk@gmail.com>
2023-05-13 20:05:02 -0300

    metodos desplazar

--- Ejercicio4/ejercicio4.py
+++ Ejercicio4/ejercicio4.py
@@ -2,8 +2,8 @@ class Ventana:
     __titulo: str
     __x_vsi: 0
     __y_vsi: 0
-    __x_vid: 500
-    __y_vid: 500
+    __x_vid: 50
+    __y_vid: 50
     #Constructor
     def __init__(self, titulo: str, x_vsi: float, y_vsi: float, x_vid: float, y_vid: float):
         self.__titulo = titulo
@@ -24,13 +24,33 @@ class Ventana:
     def mostrar(self):
         return f"(X del vertice superior izquierdo = {self.__x_vsi}; Y del vertice superior izquiero = {self.__y_vsi}; X del vertice inferior derecho = {self.__x_vid}; Y del vertice inferior derecho = {self.__y_vid})"
     def moverDerecha(self, numero):
-        
+        x = self.__x_vid + numero
+        if x < 500:
+            self.__x_vid = x
+            self.__x_vsi = self.__x_vsi + numero            
+        else:
+            print(f"No se puede mover {numero} pasos a la derecha")
     def moverIzquierda(self, numero):
-        
+        x = self.__x_vsi - numero
+        if x >= 0:
+            self.__x_vsi = x
+            self.__x_vid = self.__x_vid - numero
+        else:
+            print(f"No se puede mover {numero} pasos a la izquierda")
     def bajar(self, numero):
-        
+        x = self.__y_vid + numero
+        if x < 500:
+            self.__y_vid = x
+            self.__y_vsi = self.__y_vsi + numero            
+        else:
+            print(f"No se puede mover {numero} pasos hacia abajo")
     def subir(self, numero):
-    
+        x = self.__y_vsi - numero
+        if x >= 0:
+            self.__y_vsi = x
+            self.__y_vid = self.__y_vid - numero
+        else:
+            print(f"No se puede mover {numero} pasos hacia arriba")    
         
 if __name__ ==  '__main__':
     print('==== Ventana Inicio ====')
